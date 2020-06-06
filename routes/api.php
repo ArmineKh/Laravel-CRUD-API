e532fdf060b0f6ccaaeccf95285ed98372981924
Armine <https://github.com/ArmineKh>
2020-06-06 11:35:07 +0400

    last commit

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 use App\Person;
 
-Route::apiResource('/person', 'PersonController');
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('/person', 'Api\v1\PersonController')
+    ->only(['show', 'destroy', 'update', 'store']);
+
+    Route::apiResource('/people', 'Api\v1\PersonController')
+        ->only('index');
+});
+
+Route::prefix('v2')->group(function(){
+    Route::apiResource('/person', 'Api\v2\PersonController')
+    ->only('show');
+
+    
+});
